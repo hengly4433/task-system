a56a320bcfe1a1472e5cab4d631aefff076d470b
Hengly Siev <hengly.s@pipay.com>
2026-02-28 23:23:38 +0700

    Fix: health check issue with guard

--- backend/src/health.controller.ts
+++ backend/src/health.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { Public } from './modules/auth/decorators/public.decorator';
 
 @ApiTags('health')
 @Controller('health')
 export class HealthController {
+  @Public()
   @Get()
   @ApiOperation({ summary: 'Check API health status' })
   check() {
